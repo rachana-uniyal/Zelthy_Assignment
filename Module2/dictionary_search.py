1ad6530d3eceef23af79b16c6a4d8d548afd8ab6
Rachana Uniyal <rachanauniyal0417@gmail.com>
2021-03-27 00:29:47 +0530

    Assignment 2 updated

--- Module2/dictionary_search.py
+++ Module2/dictionary_search.py
@@ -1,23 +1,39 @@
 import requests
 
-while 1:
-	word = input("\n Word? ")
-	if not word:
-		print("Please enter a valid input")
-		continue
+class Dictionary:
+	def __init__(self,word):
+		self.url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/"
+		self.Word = word
 
-	response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en_US/"+word)
-	
-	try:
-		dictionary = response.json()[0]
 
-		print('\033[1m' + word.capitalize()+".")
-		for meaning in dictionary['meanings']:
-			result = ''
-			partOfSpeech = meaning['partOfSpeech']
-			definitions = meaning['definitions'][0]['definition']
-			result = partOfSpeech.capitalize() +". "+definitions.capitalize()
-			print('\033[0m' + result)
-	
-	except:
-		print('\033[91m' + "Word Not Found"+ '\033[0m')
+
+	def search(self):
+		if not self.Word:
+			print("Please enter a valid input")
+			return
+
+		response = requests.get(self.url+self.Word)
+		
+		try:
+			dictionary = response.json()[0]
+
+			print('\033[1m' + self.Word.capitalize()+".")
+			for meaning in dictionary['meanings']:
+				result = ''
+				partOfSpeech = meaning['partOfSpeech']
+				definitions = meaning['definitions'][0]['definition']
+				result = partOfSpeech.capitalize() +". "+definitions.capitalize()
+				print('\033[0m' + result)
+		
+		except:
+			print('\033[91m' + "Word Not Found"+ '\033[0m')
+
+
+
+
+
+
+word = input("\n Word? ")
+
+my_dictionary = Dictionary(word)
+my_dictionary.search()
